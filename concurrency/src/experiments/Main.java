ea9764ac7c3209346a0dc85995b69943d88a06b6
Yuu-gen <zel.da.hero.yr+github@gmail.com>
2020-10-26 12:42:44 +0100

    47a added lambda-notation for

--- concurrency/src/experiments/Main.java
+++ concurrency/src/experiments/Main.java
@@ -5,14 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		Deadlocker d1 = new Deadlocker("D1");
 		Deadlocker d2 = new Deadlocker("D2");
-		new Thread(new Runnable() {
-            public void run() { d1.lock(d2); }
-        }).start(); 
-		new Thread(new Runnable() {
-	            public void run() { d2.lock(d1); }
-	        }).start(); 
-		
+		new Thread(() -> d1.lock(d2)).start();
+		new Thread(() -> d2.lock(d1)).start();
+
 		System.out.println("Deadlock...");
-	       
-}
+
+	}
 }
